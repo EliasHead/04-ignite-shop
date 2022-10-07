24d6512a363ed271bf4abecc5d0383e116be16e3
eliashead <eliashead.costa@gmail.com>
2022-10-06 21:17:44 -0300

    add page success

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -36,6 +36,7 @@ export default function Home({ products }: HomeProps) {
           <Link
             href={`/product/${product.id}`} 
             key={product.id}
+            prefetch={false}
           >
             <Product  className="keen-slider__slide">
               <Image src={product.imagemUrl} width={520} height={480} alt="" />
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,38 +1,69 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import Image from 'next/future/image'
 import Stripe from "stripe";
 import { stripe } from "../../lib/stripe";
 import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product";
 
+import axios from 'axios'
+import { useState } from "react";
+
 interface ProductProps {
   product: {
     id: string;
     name: string;
-    imageURL: string;
+    imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   }
 }
 
 export default function Product({ product }: ProductProps) {
+  const [isCreatingCheckoutSession, setIsCreatingCheckoutSession] = useState(false)
+
+  async function handleBuyProduct() {
+    try {
+      setIsCreatingCheckoutSession(true)
+
+      const response = await axios.post('/api/checkout', {
+        priceId: product.defaultPriceId,
+      })
+
+      const { checkoutUrl } = response.data
+      window.location.href = checkoutUrl
+    } catch (error) {
+      // conectar com uma ferramenta de observabilidade (Datadog / sentry)
+      setIsCreatingCheckoutSession(false)
+
+      alert('Falha ao redirecionar ao checkout!')
+    }
+    console.log(product.defaultPriceId)
+  }
 
   return (
     <ProductContainer>
       <ImageContainer>
-        <Image src={product.imageURL} width={520} height={480} alt="" />
+        <Image src={product.imageUrl} width={520} height={480} alt="" />
       </ImageContainer>
       <ProductDetails>
         <h1>{product.name}</h1>
         <span>{product.price}</span>
 
         <p>{product.description}</p>
-        <button>
+        <button disabled={isCreatingCheckoutSession} onClick={handleBuyProduct}>
           Comprar agora
         </button>
       </ProductDetails>
     </ProductContainer>
   )
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
+} 
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
   const productId = params.id
@@ -53,6 +84,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ para
           currency: 'BRL',
         }).format(price.unit_amount / 100),
         description: product.description,
+        defaultPriceId: price.id,
       }
     },
     revalidate: 60 * 60 * 1, // 1 hour
